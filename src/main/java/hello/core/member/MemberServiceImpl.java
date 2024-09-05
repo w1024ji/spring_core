b01be50f1e47f320bd024a9a1f8479d814889b52
Wonji Lee <w1024ji@gmail.com>
2024-09-05 16:24:43 +0900

    @Component와 @Autowired로 쉽게 빈을 등록해보자

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,9 +1,14 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     // 얘는 자기가 뭐를 받을 지 몰라. 오로지 인터페이스만 있다. 추상화에만 의존. DIP 지키기
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,13 +5,17 @@ import hello.core.discount.FixDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import hello.core.member.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
     // OrderServiceImpl은 뭐가 들어올지 몰라. 구체적인 클래스 몰라
     private final MemberRepository memberRepository;
     private final DiscountPolicy discountPolicy;
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
