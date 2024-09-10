cd88fec7eee0f1f664cd9257761e92b0dd50f2f6
Wonji Lee <w1024ji@gmail.com>
2024-09-10 15:06:48 +0900

    @Bean에 직접 설정할 수도, 상위 클래스를 구현할 수도

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -6,9 +6,6 @@ public class NetworkClient {
 
     public NetworkClient(){
         System.out.println("생성자 호출, url = " + url);
-        connect();
-        call("초기화 연결 메시지");
-
     }
 
     public void setUrl(String url){
@@ -27,5 +24,16 @@ public class NetworkClient {
     // 서비스 종료 시 호출
     public void disconnect(){
         System.out.println("close: " + url);
+    }
+
+    public void init() {
+        System.out.println("init()");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    public void close() {
+        System.out.println("close()");
+        disconnect();
     }
 }
